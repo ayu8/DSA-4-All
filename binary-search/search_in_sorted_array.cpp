4d2b3ea10fa5d27f36b4fc0c5888c5bbec9da0d9
Ayush Singh <singhayush873@gmail.com>
2022-09-30 14:51:40 +0530

    first file

---
+++ binary-search/search_in_sorted_array.cpp
@@ -0,0 +1,20 @@
+int searchInSorted(int a[], int n, int x) {
+    int start= 0, end = n-1; 
+
+    while(start <= end){
+        int mid = (start+end)/2; 
+            
+        if(a[mid] == x) {
+            return 1; 
+        }
+            
+        else if(a[mid] > x) {
+            end = mid-1; 
+        }
+          
+        else 
+            start = mid+1; 
+    }
+        
+    return -1; 
+}
