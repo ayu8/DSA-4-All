a36eccf8268bf040449c46af23dd367672d15d37
Abhishek <55457501+Abhishek-Dobliyal@users.noreply.github.com>
2022-10-03 16:38:17 +0530

    Create koko_eating_banana.py

---
+++ binary-search/koko_eating_banana.py
@@ -0,0 +1,16 @@
+"""
+Ques Link: https://leetcode.com/problems/koko-eating-bananas/
+"""
+from math import ceil
+
+class Solution:
+    def minEatingSpeed(self, piles, H):
+        l, r = 0, max(piles)
+        while l + 1 < r:
+            mid = (l + r) // 2
+            if sum(ceil(i / mid) for i in piles) > H:
+                beg = mid
+            else:
+                end = mid
+                
+        return end
