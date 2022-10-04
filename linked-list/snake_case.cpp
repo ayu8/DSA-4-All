acd04fc1fc18b431382234320cbf0e432aa4a31d
Priya272 <76405294+Priya272@users.noreply.github.com>
2022-10-04 18:33:00 +0530

    Create snake_case.cpp
    A C++ Code

---
+++ linked-list/snake_case.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(head==NULL)
+            return head;
+        ListNode* temp=head;
+        while(temp->next!=NULL)
+        {
+            if(temp->val!=temp->next->val)
+            {
+                temp=temp->next;
+            }
+            else
+            {
+                temp->next=temp->next->next;
+            }
+        }
+        return head;
+    }
+};
+
+// Ques link:-
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
