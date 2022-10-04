f96d8b9556ab9cbc22d52e48fb61c6ced6624ec3
Ankur Nagila <73011858+ANkurNagila@users.noreply.github.com>
2022-10-04 18:37:34 +0530

    Update vector.cpp

--- STL/vector.cpp
+++ STL/vector.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-  vector<int> v1={10,20,30,40};    // vector declared with some values
+  vector<int> v1={20,40,30,10};    // vector declared with some values
   
   v1.push_back(10);                // element pushed inside vector at end
   
@@ -11,8 +11,25 @@ int main(){
   int size=v1.size();              // returns size of vector v1
   cout<<"size of vector:"<<size<<endl;
   
+  for(int i=0;i<size;i++){
+    cout<<v1[i]<<" ";              //Print elements using index
+  }
   
+  cout<<endl;
+  
+  
+  sort(v1.begin(),v1.end());       // sorts the vector
+  
+  vector<int> iterator :: it;      // Creating the iterator 
+  
+  for(it=v1.begin() ; it!=v1.end() ;it++){
+    cout<<it<<" ";                 // Using iterator to iterate over the vector
+  }
+  
+  cout<<endl;
   
   
   return 0;
 }
+
+//These are the commonly used there are other methods too.
