efc75b82a5e77b6bc1a87f414556d5498c32c878
Priya272 <76405294+Priya272@users.noreply.github.com>
2022-10-05 11:08:07 +0530

    Create remove_duplicates_from_sorted_list.cpp
    A C++ Code

---
+++ linked-list/remove_duplicates_from_sorted_list.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+
+     ListNode* temp = new ListNode(0,head);
+     ListNode* prev=temp;
+        while(head!=NULL){
+            if(head->next!=NULL && head->val == head->next->val){
+                while(head->next!=NULL && head->val == head->next->val){
+                    head=head->next;
+                }
+                prev->next = head->next;
+            }
+            else{
+                prev = prev->next;
+            }
+            head=head->next;
+        }
+        return temp->next;
+    }
+};
+
+// Ques link:-
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/
