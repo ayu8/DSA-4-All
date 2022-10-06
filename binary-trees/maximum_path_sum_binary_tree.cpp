314fa5d94fc9f41243bf09be16358ccc6f9840bb
koni24 <konisha.24.kar@gmail.com>
2022-10-06 13:08:23 +0530

    Create maximum_path_sum_binary_tree.cpp

---
+++ binary-trees/maximum_path_sum_binary_tree.cpp
@@ -0,0 +1,20 @@
+//link of the problem
+// https://leetcode.com/problems/binary-tree-maximum-path-sum/
+class Solution {
+public:
+    int findmaxpathsum(TreeNode* root, int &maxi){
+        if(root==NULL)
+            return 0;
+        int leftmaxpath=max(0,findmaxpathsum(root->left,maxi));
+        int rightmaxpath=max(0,findmaxpathsum(root->right,maxi));
+        int value=root->val;
+        maxi=max(maxi,leftmaxpath+rightmaxpath+value);
+        return max(leftmaxpath,rightmaxpath)+value;
+        
+    }
+    int maxPathSum(TreeNode* root) {
+        int maxi=INT_MIN;
+        findmaxpathsum(root,maxi);
+        return maxi;
+    }
+};
