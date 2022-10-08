ef096134c58dfc70197616ae3cfc221ca3d4df74
palak100 <72958170+palak100@users.noreply.github.com>
2022-10-08 22:42:36 +0530

    Update segregate_even_odd_nodes_in_ll.cpp

--- linked-list/segregate_even_odd_nodes_in_ll.cpp
+++ linked-list/segregate_even_odd_nodes_in_ll.cpp
@@ -1,9 +1,5 @@
-// C++ program to segregate even and
-// odd nodes in a Linked List
-#include <bits/stdc++.h>
-using namespace std;
+//GFG code Link : - https://www.geeksforgeeks.org/segregate-even-and-odd-elements-in-a-linked-list/
 
-/* a node of the singly linked list */
 class Node
 {
 	public:
@@ -46,8 +42,7 @@ Node* segregateEvenOdd(Node *head){
         if(even) return even;
         return odd;
 }
-/* UTILITY FUNCTIONS */
-/* Function to insert a node at the beginning */
+
 void push(Node** head_ref, int new_data)
 {
 	/* allocate node */
@@ -63,7 +58,7 @@ void push(Node** head_ref, int new_data)
 	(*head_ref) = new_node;
 }
 
-/* Function to print nodes in a given linked list */
+
 void printList(Node *node)
 {
 	while (node != NULL)
@@ -72,33 +67,3 @@ void printList(Node *node)
 		node = node->next;
 	}
 }
-
-/* Driver code*/
-int main()
-{
-	/* Start with the empty list */
-	Node* head = NULL;
-
-	/* Let us create a sample linked list as following
-	0->2->4->6->8->10->11 */
-
-	push(&head, 11);
-	push(&head, 10);
-	push(&head, 8);
-	push(&head, 6);
-	push(&head, 4);
-	push(&head, 2);
-	push(&head, 0);
-
-	cout << "Original Linked list ";
-	printList(head);
-
-	segregateEvenOdd(head);
-
-	cout << "\nModified Linked list ";
-	printList(head);
-
-	return 0;
-}
-
-// This code is contributed by rathbhupendra
