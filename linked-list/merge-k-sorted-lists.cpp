43c409f26909b50af67c2e6c7c82ebc74e289a23
unknown <ay34641@gmail.com>
2022-10-09 14:20:57 +0530

    update

--- linked-list/merge-k-sorted-lists.cpp
+++ linked-list/merge-k-sorted-lists.cpp
@@ -1,91 +1,128 @@
-// Problem LINK: https://leetcode.com/problems/merge-k-sorted-lists/
+// Problem Link : https://www.geeksforgeeks.org/merge-k-sorted-linked-lists/
 
-// Problem Statement
-/*
-You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
-Merge all the linked-lists into one sorted linked-list and return it.
+// Problem Statement :
 
- 
+// Given K sorted linked lists of size N each, merge them and print the sorted output.
 
-Example 1:
+// Examples: 
 
-Input: lists = [[1,4,5],[1,3,4],[2,6]]
-Output: [1,1,2,3,4,4,5,6]
-Explanation: The linked-lists are:
-[
-  1->4->5,
-  1->3->4,
-  2->6
-]
-merging them into one sorted list:
-1->1->2->3->4->4->5->6
-Example 2:
+// Input: k = 3, n =  4
+// list1 = 1->3->5->7->NULL
+// list2 = 2->4->6->8->NULL
+// list3 = 0->9->10->11->NULL
 
-Input: lists = []
-Output: []
-Example 3:
+// Output: 0->1->2->3->4->5->6->7->8->9->10->11
+// Merged lists in a sorted order 
+// where every element is greater 
+// than the previous element.
 
-Input: lists = [[]]
-Output: []
- 
+// Input: k = 3, n =  3
+// list1 = 1->3->7->NULL
+// list2 = 2->4->8->NULL
+// list3 = 9->10->11->NULL
 
-Constraints:
+// Output: 1->2->3->4->7->8->9->10->11
+// Merged lists in a sorted order 
+// where every element is greater 
+// than the previous element.
 
-k == lists.length
-0 <= k <= 104
-0 <= lists[i].length <= 500
--104 <= lists[i][j] <= 104
-lists[i] is sorted in ascending order.
-The sum of lists[i].length will not exceed 104.
-
-*/
-
-/* Algorithm Explained with Complexity
-During 1st iteration, we merge two lists(every list is N) into one, this will make K down to K / 2.
-
-During 2nd iteration, we merge two lists(every list is 2N) into one, this will make K / 2 down to k /4.
-
-During 3rd iteration, we merge two lists(every list is 4N) into one, this will make K / 4 down to k /8.
-
-And so forth...
-
-I think when we merge two lists, the complexity is O(list1.length) + O(list2.length).
-
-So the total complexity is:
-
-(2N) * (K / 2) + 
-(4N) * (K / 4) + 
-(8N) * (K / 8) + 
-.............. + 
-(2^logK*N) * (K / (2 ^logK)) 
-
-= logK*KN
-*/
+// *****************************************************************************************
+// Code
 
 
+#include <bits/stdc++.h>
+using namespace std;
 
+struct Node {
+	int data;
+	Node* next;
+};
 
-ListNode *mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (NULL == l1) return l2;
-        else if (NULL == l2) return l1;
-        if (l1->val <= l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        }
-        else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
-    }
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        if (lists.empty()) return NULL;
-        int len = lists.size();
-        while (len > 1) {
-            for (int i = 0; i < len / 2; ++i) {
-                lists[i] = mergeTwoLists(lists[i], lists[len - 1 - i]);
-            }
-            len = (len + 1) / 2;
-        }
-        
-        return lists.front();
-    }
+void printList(Node* node)
+{
+	while (node != NULL) {
+		printf("%d ", node->data);
+		node = node->next;
+	}
+}
+
+Node* mergeKLists(Node* arr[], int last)
+{
+	for (int i = 1; i <= last; i++) {
+		while (true) {
+			Node *head_0 = arr[0], *head_i = arr[i];
+			if (head_i == NULL)
+				break;
+
+			if (head_0->data >= head_i->data) {
+				arr[i] = head_i->next;
+				head_i->next = head_0;
+				arr[0] = head_i;
+			}
+			else
+				while (head_0->next != NULL) {
+					if (head_0->next->data
+						>= head_i->data) {
+						arr[i] = head_i->next;
+						head_i->next = head_0->next;
+						head_0->next = head_i;
+						break;
+					}
+					head_0 = head_0->next;
+
+					if (head_0->next == NULL) {
+						arr[i] = head_i->next;
+						head_i->next = NULL;
+						head_0->next = head_i;
+						head_0->next->next = NULL;
+						break;
+					}
+				}
+		}
+	}
+
+	return arr[0];
+}
+
+Node* newNode(int data)
+{
+	struct Node* temp = new Node;
+	temp->data = data;
+	temp->next = NULL;
+	return temp;
+}
+
+int main()
+{
+	// Number of linked lists
+	int k = 3;
+
+	// Number of elements in each list
+	int n = 4;
+
+	// an array of pointers storing the
+	// head nodes of the linked lists
+	Node* arr[k];
+
+	arr[0] = newNode(1);
+	arr[0]->next = newNode(3);
+	arr[0]->next->next = newNode(5);
+	arr[0]->next->next->next = newNode(7);
+
+	arr[1] = newNode(2);
+	arr[1]->next = newNode(4);
+	arr[1]->next->next = newNode(6);
+	arr[1]->next->next->next = newNode(8);
+
+	arr[2] = newNode(0);
+	arr[2]->next = newNode(9);
+	arr[2]->next->next = newNode(10);
+	arr[2]->next->next->next = newNode(11);
+
+	// Merge all lists
+	Node* head = mergeKLists(arr, k - 1);
+
+	printList(head);
+
+	return 0;
+}
