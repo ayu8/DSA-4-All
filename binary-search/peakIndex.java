2a17502d9c50aff7a12cb6774f9b2f44f4dc36e7
Darshan Jain <108680813+darshanjain-entrepreneur@users.noreply.github.com>
2022-10-09 18:41:09 +0530

    peakindex question in java
    it is very important question in binary search

---
+++ binary-search/peakIndex.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        
+        int start = 0;
+        int end = arr.length - 1;
+        while(start < end){
+            int mid = start + (end -start)/2;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
+            }else{
+                start = mid + 1;
+            }
+            
+        }
+        return start;
+    }
+}
