d3e6232daeb07a26f938215c75b762ea9825f2b5
madhav-jpg <50106950+madhav-jpg@users.noreply.github.com>
2022-10-25 11:52:10 +0530

    Update make_a_change.cpp

--- greedy/make_a_change.cpp
+++ greedy/make_a_change.cpp
@@ -1,21 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+vector<int> make_a_change(vector<int> denomination,int amount)
 {
-    vector<int> denomination,coinCount;
+    vector<int> coinCount;
     vector<int>::iterator itr;
-    int d,amount;
-    cin>>d;
-    while(d>0)                      //Storing different denomination until 0
-    {
-        denomination.push_back(d);
-        cin>>d;
-    }
-    sort(denomination.begin(),denomination.end(),greater<int>());
-    
-    cout<<"Amount : ";
-    cin>>amount;                    //Storing total amount
     
     for(itr=denomination.begin();itr!=denomination.end();itr++)
     {
@@ -23,9 +12,6 @@ int main()
         amount %= (*itr);
     }
     
-    for(int i=0;i<denomination.size();i++)
-    {
-        cout<<denomination[i]<<"\t"<<coinCount[i]<<endl;        //Printing result
-    }
-    return 0;
+    return coinCount;                                           //Returning result
 }
+//Algorithm Link : https://www.log2base2.com/algorithms/greedy/greedy-algorithm.html
